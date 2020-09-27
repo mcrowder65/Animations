d26862938dcb408e4c8a99c7d679925b9a47da75
Matt Crowder <10186520+mcrowder65@users.noreply.github.com>
2020-09-27 13:20:00 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ExampleButton from "src/components/example-button"
+function App() {
+  return (
+    <div>
+      <ExampleButton label="Button" />
+    </div>
+  )
+}
+
+export default App
